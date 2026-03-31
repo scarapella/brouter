6c30d57b9257538aae65bd2268d9e39f2e84d2e4
Sean P. Carapella <73687678+scarapella@users.noreply.github.com>
2026-03-31 10:57:13 -0400

    Update mv_segments.sh to be generic, relative paths

--- misc/scripts/mapcreation/mv_segments.sh
+++ misc/scripts/mapcreation/mv_segments.sh
@@ -1,4 +1,3 @@
 set -x
-#rm /home/scarapella/dev/brouter/brouter/misc/segments4/*
-cp /home/scarapella/dev/brouter/brouter/misc/scripts/mapcreation/tmp/segments/* /home/scarapella/dev/brouter/brouter/misc/segments4/
-
+mkdir ../../segments4/
+cp tmp/segments/* ../../segments4/
