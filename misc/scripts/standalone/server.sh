1957d9d56cc6d2bad39a30add5d286f578944807
scarapella <sean@carapella.net>
2026-04-02 19:48:09 -0400

    add buffered reader

--- misc/scripts/standalone/server.sh
+++ misc/scripts/standalone/server.sh
@@ -4,8 +4,10 @@ cd "$(dirname "$0")"
 # BRouter standalone server
 # java -cp brouter.jar btools.brouter.RouteServer <segmentdir> <profile-map> <customprofiledir> <port> <maxthreads> [bindaddress]
 
+# use useBufferedReader=true to enable buffered reading of RD5 files (useful for high latency storage like cloud buckets)
+# optionally use readBufferSize=2m configure the buffer size (default is 1M)
 # maxRunningTime is the request timeout in seconds, set to 0 to disable timeout
-JAVA_OPTS="-Xmx128M -Xms128M -Xmn8M -DmaxRunningTime=300 -DuseRFCMimeType=false"
+JAVA_OPTS="-Xmx128M -Xms128M -Xmn8M -DmaxRunningTime=300 -DuseRFCMimeType=false -DuseBufferedReader=true"
 
 # If paths are unset, first search in locations matching the directory structure
 # as found in the official BRouter zip archive
